28f79a52601230e66425d293ed8de2ea6523f018
hedaukartik <hedaukartik2009@gmail.com>
2020-10-19 00:15:06 +0530

    FE aboutUsContact nav

--- client/src/layouts/AboutLayout.js
+++ client/src/layouts/AboutLayout.js
@@ -3,6 +3,8 @@ import MetaTags from "react-meta-tags";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import Breadcrumb from "../wrappers/breadcrumb/Breadcrumb";
 import AboutUsOne from "../components/about-us/AboutUsOne";
+import AboutUsContact from "../components/about-us/AboutUsContact"
+
 
 const AboutLayout = ({ location }) => {
     const { pathname } = location;
@@ -11,7 +13,7 @@ const AboutLayout = ({ location }) => {
 	return (
 		<Fragment>
 			<MetaTags>
-				<title>Ecom | Design Home</title>
+				<title>Ecom | Design About Us</title>
 				<meta
 					name="description"
 					content="Design home of Ecom eCommerce."
@@ -21,6 +23,7 @@ const AboutLayout = ({ location }) => {
 			<BreadcrumbsItem to={`${pathname}`}>{layoutName}</BreadcrumbsItem>
             <Breadcrumb layoutName={layoutName} bgImageUrl={bgImageUrl}/>
             <AboutUsOne />
+            <AboutUsContact />
 		</Fragment>
 	);
 };
